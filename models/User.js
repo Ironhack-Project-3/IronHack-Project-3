70efe87f8efa623bf2fa5bf18c66ca6dea1a67eb
Benoit Hage <hage.benoit@gmail.com>
2020-07-22 13:26:19 +0200

    schema is the making

---
+++ models/User.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    index: {
+      unique: true
+    }
+  },
+  password: {
+    type: String,
+    required: true,
+    index: {
+      unique: true
+    }
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  role: {
+    type: String,
+    required: true
+  },
+  incompetence: {
+    type: String,
+    enum: ['speaking', 'writing', 'walking'],
+    default: 'speaking',
+    required: true
+  },
+  competence: {
+    type: String,
+    enum: ['speaking', 'writing', 'walking'],
+    default: 'speaking',
+    required: true
+  },
+  age: Number,
+  address: {
+    type: String,
+    required: true
+  },
+  picture: String,
+  biography: String,
+  review: String
+});
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
