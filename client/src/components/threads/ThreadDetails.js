8038b00fa9db5b669adeaf10175823eaff4be019
nina <ninasmolnikova@gmail.com>
2020-07-24 14:59:03 +0200

    d

--- client/src/components/threads/ThreadDetails.js
+++ client/src/components/threads/ThreadDetails.js
@@ -5,46 +5,45 @@ import { Link, Redirect } from 'react-router-dom';
 import ThreadList from './ThreadList'
 
 class ThreadDetails extends Component {
- 
-    state = {
-    thread: []
-    }
 
-    componentDidMount = () => {
-      axios.get("/api/threads/" + this.props.match.params.id)
+  state = {
+    thread: []
+  }
+
+  componentDidMount = () => {
+    axios.get("/api/threads/" + this.props.match.params.id)
       .then(threadObject => {
         console.log(threadObject)
         this.setState({
           thread: threadObject.data
-        }) 
-      } )
-    }
-
-  render(){
- 
-    console.log(this.props)
- 
-if (this.state.thread) {
-    return     <div className="thread-details">
-      <div className="thread-details-text">
-
-<h3>{this.state.thread.title}</h3>
-<p>{this.state.thread.description}</p>
-<div className="thread-details-buttons">
-
-<Link to={`/threads/${this.state.thread._id}`}>
-                  <p>Contact meat</p>
-                </Link>
-                <Link to={`/threads/${this.state.thread._id}`}>
-                  <p>Add to favourites</p>
-                </Link>
-</div>
-</div>
-    </div>
-    
+        })
+      })
   }
-}
+
+  render() {
+
+    console.log(this.props)
+
+    if (this.state.thread) {
+      return <div className="thread-details">
+        <div className="thread-details-text">
+
+          <h3>{this.state.thread.title}</h3>
+          <p>{this.state.thread.description}</p>
+          <div className="thread-details-buttons">
+
+            <Link to={`/threads/${this.state.thread._id}`}>
+              <p>Contact meat</p>
+            </Link>
+            <Link to={`/threads/${this.state.thread._id}`}>
+              <p>Add to favourites</p>
+            </Link>
+          </div>
+        </div>
+      </div>
+
+    }
+  }
 }
 
 export default ThreadDetails;
- 
