ba22d4c71616404da688f3d20811e60e4bcabd4f
nina <ninasmolnikova@gmail.com>
2020-07-24 14:38:32 +0200

    initial styling for threads

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -7,8 +7,7 @@ const Navbar = () => {
       <ul>
         <li><Link to='/'>Home</Link></li>
         <li><Link to='/Profile'>Profile</Link></li>
-
-        <li><Link to='/threads'>Threads</Link></li>
+ 
       </ul>
     </nav>
   )
