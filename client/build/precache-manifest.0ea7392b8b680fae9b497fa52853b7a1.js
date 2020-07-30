186f560f964450d3490ebb81b392d8ad53b10f1e
Benoit Hage <hage.benoit@gmail.com>
2020-07-30 15:31:31 +0200

    merde

--- client/build/precache-manifest.0ea7392b8b680fae9b497fa52853b7a1.js
+++ client/build/precache-manifest.0ea7392b8b680fae9b497fa52853b7a1.js
@@ -1,31 +1,35 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a520f8a297815fd0c496550b63127193",
+    "revision": "58ca16864cee475895684d421438e2b0",
     "url": "/index.html"
   },
   {
-    "revision": "6ca2d11433e985a19331",
+    "revision": "649fb639c36f265138f1",
     "url": "/static/css/2.deaf9b8d.chunk.css"
   },
   {
-    "revision": "e2462f4cf1501138a07c",
-    "url": "/static/css/main.e754f9e7.chunk.css"
+    "revision": "282a42ccd355bbd80277",
+    "url": "/static/css/main.55028c0c.chunk.css"
   },
   {
-    "revision": "6ca2d11433e985a19331",
-    "url": "/static/js/2.f9f20657.chunk.js"
+    "revision": "649fb639c36f265138f1",
+    "url": "/static/js/2.322c9c23.chunk.js"
   },
   {
     "revision": "ebec6ec5880fcec562f9a7747ae47b51",
-    "url": "/static/js/2.f9f20657.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.322c9c23.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "e2462f4cf1501138a07c",
-    "url": "/static/js/main.d3502ef3.chunk.js"
+    "revision": "282a42ccd355bbd80277",
+    "url": "/static/js/main.e1dbe5a6.chunk.js"
   },
   {
     "revision": "9b4e396c183e42c1fa5c",
     "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "045646f406c3d36c8c6d3aa0c5735fbf",
+    "url": "/static/media/banner-06-06.045646f4.png"
   },
   {
     "revision": "62026d4f33e94c34f74283cfe1099afb",
@@ -34,10 +38,6 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "fbcc16813dc2287d9f9041675538f103",
     "url": "/static/media/bridge.fbcc1681.png"
-  },
-  {
-    "revision": "934146e21a55295fca4b3742500e2ddd",
-    "url": "/static/media/layout.934146e2.jpg"
   },
   {
     "revision": "966fdaf87c7f72a2f022dda7ded12a4e",
