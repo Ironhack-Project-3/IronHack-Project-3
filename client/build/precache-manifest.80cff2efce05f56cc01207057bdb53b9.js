278b0ce3b92b3a9f935c1776b43e293f807001eb
Benoit Hage <hage.benoit@gmail.com>
2020-07-30 12:14:22 +0200

    deploy?

---
+++ client/build/precache-manifest.80cff2efce05f56cc01207057bdb53b9.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a520f8a297815fd0c496550b63127193",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ca2d11433e985a19331",
+    "url": "/static/css/2.deaf9b8d.chunk.css"
+  },
+  {
+    "revision": "e2462f4cf1501138a07c",
+    "url": "/static/css/main.e754f9e7.chunk.css"
+  },
+  {
+    "revision": "6ca2d11433e985a19331",
+    "url": "/static/js/2.f9f20657.chunk.js"
+  },
+  {
+    "revision": "ebec6ec5880fcec562f9a7747ae47b51",
+    "url": "/static/js/2.f9f20657.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2462f4cf1501138a07c",
+    "url": "/static/js/main.d3502ef3.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "62026d4f33e94c34f74283cfe1099afb",
+    "url": "/static/media/bear.62026d4f.png"
+  },
+  {
+    "revision": "fbcc16813dc2287d9f9041675538f103",
+    "url": "/static/media/bridge.fbcc1681.png"
+  },
+  {
+    "revision": "934146e21a55295fca4b3742500e2ddd",
+    "url": "/static/media/layout.934146e2.jpg"
+  },
+  {
+    "revision": "966fdaf87c7f72a2f022dda7ded12a4e",
+    "url": "/static/media/people.966fdaf8.png"
+  },
+  {
+    "revision": "eb16a9b7b64c0b1613256fb344cf095f",
+    "url": "/static/media/tower.eb16a9b7.png"
+  },
+  {
+    "revision": "a04120c52fbd13f05ab00daa3b72e810",
+    "url": "/static/media/welcome-09.a04120c5.png"
+  }
+]);
