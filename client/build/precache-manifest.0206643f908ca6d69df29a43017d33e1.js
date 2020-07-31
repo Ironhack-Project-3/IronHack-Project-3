f63a6d0e63304139ecbaeaa393ee6aec84bd3c1a
nina <ninasmolnikova@gmail.com>
2020-07-31 13:29:03 +0200

    whatever

---
+++ client/build/precache-manifest.0206643f908ca6d69df29a43017d33e1.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cde400054374b9c42bba2e2529d67806",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c36e33839ae0e935514c",
+    "url": "/static/css/2.deaf9b8d.chunk.css"
+  },
+  {
+    "revision": "37f423a62420539bacdf",
+    "url": "/static/css/main.64a1d3c9.chunk.css"
+  },
+  {
+    "revision": "c36e33839ae0e935514c",
+    "url": "/static/js/2.7231dc01.chunk.js"
+  },
+  {
+    "revision": "ebec6ec5880fcec562f9a7747ae47b51",
+    "url": "/static/js/2.7231dc01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37f423a62420539bacdf",
+    "url": "/static/js/main.a2ff5567.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "5fb4aa372b0eaf3212ca8ad838dd8823",
+    "url": "/static/media/Four.5fb4aa37.png"
+  },
+  {
+    "revision": "62026d4f33e94c34f74283cfe1099afb",
+    "url": "/static/media/bear.62026d4f.png"
+  },
+  {
+    "revision": "fbcc16813dc2287d9f9041675538f103",
+    "url": "/static/media/bridge.fbcc1681.png"
+  },
+  {
+    "revision": "a481c13956806e96208c4676d8f579d1",
+    "url": "/static/media/logo-09.a481c139.png"
+  },
+  {
+    "revision": "34ce138c47c537656e9ba75f7470c37b",
+    "url": "/static/media/new banner-crop.34ce138c.png"
+  },
+  {
+    "revision": "966fdaf87c7f72a2f022dda7ded12a4e",
+    "url": "/static/media/people.966fdaf8.png"
+  },
+  {
+    "revision": "eb16a9b7b64c0b1613256fb344cf095f",
+    "url": "/static/media/tower.eb16a9b7.png"
+  },
+  {
+    "revision": "a04120c52fbd13f05ab00daa3b72e810",
+    "url": "/static/media/welcome-09.a04120c5.png"
+  }
+]);
